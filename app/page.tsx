01aa051d774b39a1371e0d5e04440481bf4aca8f
aanubhavv <anubhavg60@gmail.com>
2025-11-12 18:41:52 +0530

    fixed indentation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50 underline">
-            Hello World
-          </h1>
-    </div>
-  );
+
+	
+
+	return (
+		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+			<h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50 underline">
+				Hello World
+			</h1>
+		</div>
+	);
 }
